c0fc865c3f7dfb6c9b20678ee9466659875b623f
yibre <cdoyue@gmail.com>
2019-12-01 20:41:31 +0900

    1-1 comments added

--- prob2EX1.py
+++ prob2EX1.py
@@ -1,16 +1,25 @@
 import argparse
 import pickle as pk
 
+tables_ex = {'A': [-1, 339, 165, 0, 35, 59, 203, 785, 867, 225, 905],
+             'B': [-1, 9, 818, 510, 400, 997, 341, 549, 412, 294, 185]}
 
+# print(tables_ex['A'][1])
 # output format = list, [n날의 fish 의 수, char type n날 A 호수인지 B 호수인지]
 # ex) [int, 'A'], [int, 'B']
+
+count = [0]
 def DP_Fish(tables, L, n):
+    count[0] = count[0]+1
     fishA = tables['A'][n]
+    #print(len(tables['A'])) = 100
     fishB = tables['B'][n]
-    fishBefore = DP_Fish(tables, L, n - 1)[0]
-    lakeBefore = DP_Fish(tables, L, n - 1)[1]
+
     if (n == 0):
         return [0, 'A']
+
+    fishBefore = DP_Fish(tables, L, n - 1)[0]
+    lakeBefore = DP_Fish(tables, L, n - 1)[1]
 
     if (abs(fishA - fishB) > L):  # ex: tax = 5, (A,B) = (1, 9), (2, 8)
         # tax를 지불하는게 그대로 남아있는 것보다 크면 이동해야함
@@ -49,6 +58,8 @@ if __name__ == "__main__":
     tables = pk.load(f)
     f.close()
 
-    result = DP_Fish(tables, L, n)[0]  # 수정함, 리스트 형식으로 output을 고침
+    result = DP_Fish(tables, L, n)[0]
+    # 수정함, 리스트 형식으로 output을 고침
 
     print("L : [{:d}] n : [{:d}] result : [{:d}]".format(L, n, result))
+    print(count[0])
